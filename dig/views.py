210c57ef09db984e66987ce5e1b0789caa65a671
Rashed Chowdhury <rchowdhury@acmeunited.com>
2021-08-20 17:38:39 -0400

    Modified ajax to show loading screen

--- dig/views.py
+++ dig/views.py
@@ -13,13 +13,43 @@ def home(request):
 def do_dig(request):
     website = request.GET.get('website_port')
     record_type = request.GET.get('record_type')
-    print(website)
-    print(record_type)
-    answers = resolver.query(website, record_type)
     dig_result = []
-    for rdata in answers:
-        dig_result.append(rdata)
+    if record_type == "ANY":
+        answer_a = resolver.query(website, "A")
+        for rdata in answer_a:
+            dig_result.append(rdata)
+        try:
+            answer_aaaa = resolver.query(website, "AAAA")
+            for rdata in answer_aaaa:
+                dig_result.append(rdata)
+        except:
+            pass
+        answer_mx = resolver.query(website, "MX")
+        for rdata in answer_mx:
+            dig_result.append(rdata)
+        answer_NS = resolver.query(website, "NS")
+        for rdata in answer_NS:
+            dig_result.append(rdata)
+        answer_TXT = resolver.query(website, "TXT")
+        for rdata in answer_TXT:
+            dig_result.append(rdata)
+        answer_SOA = resolver.query(website, "SOA")
+        for rdata in answer_SOA:
+            dig_result.append(rdata)
+        try:
+            answer_CAA = resolver.query(website, "CAA")
+            for rdata in answer_CAA:
+                dig_result.append(rdata)
+        except:
+            pass
+        
+        answers= dig_result
+    else:
+        try:
+            answers = resolver.query(website, record_type)
+        except:
+            answers = "No record found!"
+
         #print('Host', rdata.exchange, 'has preference', rdata.preference)
-    print (HttpResponse(answers))
     return HttpResponse(answers)
     #return render(request, 'polls/dig-result.html', {'dnsinfo':dig_result})
