a638d4ceff254d041fc3087a99fb6131b9607a7f
Rashed Chowdhury <rchowdhury@acmeunited.com>
2021-10-25 17:56:35 -0400

    Tailwind-css install

--- sslally_site/settings.py
+++ sslally_site/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'polls',
     'ping',
     'dig',
+    #Other django apps
+    'tailwind',
+    'theme'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TAILWIND_APP_NAME = 'theme'
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
