c6b1427a32553c55eb634aa25d4386c0b2440bee
Rashed Chowdhury <rchowdhury@acmeunited.com>
2022-04-26 16:11:31 -0400

    modified port check html and views.py

--- portcheck/views.py
+++ portcheck/views.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render
 from django.http.response import HttpResponse, JsonResponse
 from modules.icmplib import *
@@ -17,7 +18,10 @@ def get_host_port(website):
     website = website
     if ":" in website:
         port = website.split(":")[1]  # Get port number
-        website = website.split(":")[0]  # All exepct port number and colon
+        website = website.split(":")[0]
+    else:
+        website = website
+        port = 443  # All exepct port number and colon
     return(website, port)
 
 
@@ -27,14 +31,18 @@ def get_host_port(website):
 
 def get_port_status(request):
     website = request.GET.get('website_port')
-    websiet_port = get_host_port(website)
-    host = websiet_port[0]
-    port = int(websiet_port[1])
-    print(host, port)
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        sock.settimeout(2)
-        if sock.connect_ex((host, port)) == 0:
-            result = f"Port {port} is open"
-        else:
-            result = f"Port {port} is closed"
-    return JsonResponse(result, safe=False)
+    if website != "" and (".") in website:
+        websiet_port = get_host_port(website)
+        host = websiet_port[0]
+        port = int(websiet_port[1])
+        print(host, port)
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            sock.settimeout(2)
+            if sock.connect_ex((host, port)) == 0:
+                result = f"Port {port} is open"
+            else:
+                result = f"Port {port} is closed"
+        return JsonResponse(result, safe=False)
+    else:
+        print("website is empty")
+        return JsonResponse("Enter proper value", safe=False)
