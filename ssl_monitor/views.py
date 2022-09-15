501b6b6120c1c9a4716b5436e9c88d13f104aecc
Rashed Chowdhury <rchowdhury@acmeunited.com>
2022-09-15 18:21:12 -0400

    Working on monitor page

--- ssl_monitor/views.py
+++ ssl_monitor/views.py
@@ -1,6 +1,9 @@
+from http.client import HTTPResponse
+from django.http import JsonResponse
 from multiprocessing import context
 from django.shortcuts import render
 import polls.views
+import json 
 from ratelimit.decorators import ratelimit
 from ssl_monitor.models import CertMonitor #import database models
 
@@ -8,7 +11,7 @@ from ssl_monitor.models import CertMonitor #import database models
 @ratelimit(key='ip', rate='1/m')
 def home(request):
     
-    url = "nessadc.com"
+    """  url = "nessadc.com"
     certInfo = polls.views.CertInformation(url,"443")
     
 
@@ -17,7 +20,8 @@ def home(request):
     certs.checkFrequency = 3600
     certs.certValid = certInfo.getCertStatus()
     certs.expirationDate = certInfo.certExpirationDate
-    certs.save()
+    certs.save() 
+    """
     
     
 
@@ -29,3 +33,49 @@ def home(request):
     
        
     return render(request, 'polls/sslmonitor.html', context)
+
+def monitorUrl(request):
+    website = request.GET.get('website_port')
+    if website != "" and (".") in website:
+        website_port = get_host_port(website)
+        host = website_port[0]
+        port = int(website_port[1])
+        print(host, port)
+
+    certInfo = polls.views.CertInformation(host,port)
+    certs = CertMonitor()
+    certs.url = host
+    certs.checkFrequency = 3600
+    certs.certValid = certInfo.getCertStatus()
+    certs.expirationDate = certInfo.certExpirationDate
+    certs.save()
+    
+    
+
+    certObjs = list(CertMonitor.objects.values())
+
+    #print(list(CertMonitor.objects.values()))
+
+
+
+    """  context = {
+        "cert_urls": certObjs
+    } 
+    """
+    certObjs = json.dumps(certObjs)
+    print(certObjs)
+
+    #return render(request, 'polls/sslmonitor.html', context)
+    return JsonResponse(certObjs,safe = False)
+
+def get_host_port(website):
+    website = website
+    if ":" in website:
+        port = website.split(":")[1]  # Get port number
+        website = website.split(":")[0]
+    else:
+        website = website
+        port = 443  # All exepct port number and colon
+    return(website, port)
+
+
